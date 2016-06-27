fda68d875874f7d7d8d6dda6e54d179e00e7facc
layely <ablayelyfondou@gmail.com>
2016-06-27 12:44:12 +0000

    ajout de la classe Eleve

---
+++ src/objet/Eleve.java
@@ -0,0 +1,16 @@
+package objet;
+
+import java.util.GregorianCalendar;
+
+public class Eleve {
+
+	int numTable;
+	String nom;
+	String prenom;
+	String lieuNaissance;
+	GregorianCalendar dateNaissance;
+	String email;
+	Resultat resultat;
+	Ecole ecole;
+
+}

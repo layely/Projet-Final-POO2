40029e4df0ad2ca08b66b15bf1f956f425770681
layely <ablayelyfondou@gmail.com>
2016-06-27 12:27:18 +0000

    creation de la classe Ecole

---
+++ src/objet/Ecole.java
@@ -0,0 +1,33 @@
+package objet;
+
+public class Ecole {
+	String nom;
+	String motDePasse;
+
+	public Ecole(String nom, String motDePasse) {
+		this.setNom(nom);
+		this.setMotDePasse(motDePasse);
+	}
+
+
+
+	/////////////////////////////////////
+	// Getters and Setters
+	/////////////////////////////////////
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+}

47fa56e7e24d9150a3cdaa017c05446d47bfd0af
Sow Abou <abousow798@gmail.com>
2016-06-28 01:47:26 +0000

    ajout de la classe DBConnection

--- src/databaseDAOs/DBConnection.java
+++ src/databaseDAOs/DBConnection.java
@@ -1,5 +1,21 @@
 package databaseDAOs;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
 public class DBConnection {
+
+	public Connection getConnection() {
+		try {
+			Connection connect = (Connection) DriverManager.getConnection("jdbc:mysql:localhost:3306/base_de_données",
+					"root", "senegal111");
+			return connect ;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }

5bf24b2b1c02541f4390a984c13812efd8e5a5dd
layely <ablayelyfondou@gmail.com>
2016-07-04 05:27:56 +0000

    modif de DBConnection.java : mis en place de la connection vers db4free.net

--- src/databaseDAOs/DBConnection.java
+++ src/databaseDAOs/DBConnection.java
@@ -9,8 +9,8 @@ public class DBConnection {
 
 	public static Connection getConnection() {
 		try {
-			Connection connect = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/base_de_données",
-					"root", "fondouly");
+			Connection connect = (Connection) DriverManager.getConnection("jdbc:mysql://www.db4free.net:3306/orientation",
+					"orientation", "42c1c3");
 			return connect;
 		} catch (SQLException e) {
 			e.printStackTrace();

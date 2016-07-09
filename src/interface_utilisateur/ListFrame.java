a4f372c80e8f15fe8ee8cfde8c2b63c16bc703c9
layely <ablayelyfondou@gmail.com>
2016-07-09 20:27:34 +0000

    liste des ecoles : set up
    creation de EcoleModel

--- src/interface_utilisateur/ListFrame.java
+++ src/interface_utilisateur/ListFrame.java
@@ -1,13 +1,15 @@
 package interface_utilisateur;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 
 import tablemodels.EleveModel;
+import tablemodels.LyceeModel;
 
 public class ListFrame extends JFrame {
 	protected JTable table;
@@ -26,7 +28,23 @@ public class ListFrame extends JFrame {
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		initialize();
 	}
-
+	
+	public ListFrame(JFrame parent, AbstractTableModel tableModel) {
+		this.parent = parent;
+		this.setSize(400, 400);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		initialize(tableModel);
+	}
+	
+	private void initialize(AbstractTableModel tableModel) {
+		setLayout(new BorderLayout(0, 0));
+		this.table = new JTable();
+		table.setModel(tableModel);
+		table.setIntercellSpacing(new Dimension(10, 10));
+		table.setAutoCreateRowSorter(true);
+		add(new JScrollPane(table), BorderLayout.CENTER);
+	}
+	
 	private void initialize() {
 		setLayout(new BorderLayout(0, 0));
 		this.table = new JTable();
